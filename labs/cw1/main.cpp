10dba3bbd20a775d6ba6e800f22cad1fdaa12263
Babis K <esrever2357@gmail.com>
2020-10-02 01:35:25 +0100

    added coursework 1 sample project

---
+++ labs/cw1/main.cpp
@@ -0,0 +1,94 @@
+// This is a chopped Pong example from SFML examples
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main()
+{
+    std::srand(static_cast<unsigned int>(std::time(NULL)));
+
+    // example folder to load images
+    constexpr char* image_folder = "D:/photos_newest/jpg";
+    std::vector<std::string> imageFilenames;
+    for (auto& p : fs::directory_iterator(image_folder))
+        imageFilenames.push_back(p.path().u8string());
+
+    // Define some constants
+    const float pi = 3.14159f;
+    const int gameWidth = 800;
+    const int gameHeight = 600;
+
+    int imageIndex = 0;
+
+    // Create the window of the application
+    sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight, 32), "Image Fever",
+                            sf::Style::Titlebar | sf::Style::Close);
+    window.setVerticalSyncEnabled(true);
+
+    // Load an image to begin with
+    sf::Texture texture;
+    if (!texture.loadFromFile(imageFilenames[imageIndex]))
+        return EXIT_FAILURE;
+    sf::Sprite sprite (texture);
+    // I'm being lazy here and playing safe, as input images can be huge. You can do it better! To fit the screen well
+    sprite.setScale(sf::Vector2f(0.1f,0.1f));
+
+    sf::Clock clock;
+    while (window.isOpen())
+    {
+        // Handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Window closed or escape key pressed: exit
+            if ((event.type == sf::Event::Closed) ||
+               ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)))
+            {
+                window.close();
+                break;
+            }
+            
+            // Window size changed, adjust view appropriately
+            if (event.type == sf::Event::Resized)
+            {
+                sf::View view;
+                view.setSize(gameWidth, gameHeight);
+                view.setCenter(gameWidth/2.f, gameHeight/2.f);
+                window.setView(view);
+            }
+
+            // Arrow key handling!
+            if (event.type == sf::Event::KeyPressed)
+            {
+                // adjust the image index
+                if (event.key.code == sf::Keyboard::Key::Left)
+                    imageIndex = (imageIndex + imageFilenames.size() - 1) % imageFilenames.size();
+                else if (event.key.code == sf::Keyboard::Key::Right)
+                    imageIndex = (imageIndex + 1) % imageFilenames.size();
+                // ... and load the appropriate texture, and put it in the sprite
+                if (texture.loadFromFile(imageFilenames[imageIndex]))
+                {
+                    sprite = sf::Sprite(texture);
+                    sprite.setScale(sf::Vector2f(0.1f, 0.1f));
+                }
+            }
+        }
+
+        // Clear the window
+        window.clear(sf::Color(50, 200, 50));
+        // draw the sprite
+        window.draw(sprite);
+        // Display things on screen
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
